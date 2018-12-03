04b60bfa33b2a688dd36a34eafb18765977e9f6c
Justin Zwiebel <justin.zwiebel@duke.edu>
2018-12-02 22:26:06 -0500

    zwiebel autocomplete

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -84,8 +84,11 @@ public class HuffProcessor {
 
 	private void writeHeader(HuffNode root, BitOutputStream out) {
 			if(root.myLeft == null && root.myRight == null) {
+				
+				out.writeBits(1, 1);
 				out.writeBits(BITS_PER_WORD+1,root.myValue);
-			}else {
+			}
+			else {
 			out.writeBits(1, 0);
 		    writeHeader(root.myLeft, out);
 		    writeHeader(root.myRight, out);
